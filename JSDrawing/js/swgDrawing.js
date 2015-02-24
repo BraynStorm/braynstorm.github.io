d69bc67df9d649785ce6ab6a33f3d9178e7816ce
BraynStorm <glav0r3zzz4@gmail.com>
2015-02-24 22:41:32 +0200

    Added projections of SIN and COS

--- JSDrawing/js/swgDrawing.js
+++ JSDrawing/js/swgDrawing.js
@@ -1,20 +1,20 @@
 $(document).ready(function (){
-	var box			= $("#angleBox");
+	var angleBox	= $("#angleBox");
+	var radiusBox	= $("#radiusBox");
 	var beginX		= window.innerWidth/2;
 	var beginY		= window.innerHeight/2;
-	
 	
 	var sinColor = "#00AA00";
 	var cosColor = "#0000AA";
 	var tgColor = "#00AAAA";
 	var cotgColor = "#AA0000";
 	
-	
-	var radius		= 350;
+	var radius		= 100;
 	
 	var svg			= $('svg');
 	
 	var circle		= $('#mainCircle');
+	var showAdditions = $("#showAdditions");
 	
 	var sinAxis 	= $("#sinAxis");
 	var cosAxis		= $("#cosAxis");
@@ -26,94 +26,133 @@ $(document).ready(function (){
 	var tgTxt		= $("#tgTxt");
 	var cotgTxt		= $("#cotgTxt");
 	
+	
+	
 	var angleArm	= $("#angleArm");
 	var angleArc	= $("#angleArc");
 	
-	svg.css({
-		width:  window.innerWidth,
-		height: window.innerHeight
+	var projectionSin = $("#projectionSin");
+	var projectionCos = $("#projectionCos");
+	
+	radiusBox.keypress(function (){
+		init();
 	});
 	
-	circle.attr({
-		cx : beginX,
-		cy : beginY,
-		r : radius
+	function init(){
+		radius = parseInt(radiusBox.val());
+		svg.css({
+			width:  window.innerWidth,
+			height: window.innerHeight
+		});
+		
+		circle.attr({
+			cx : beginX,
+			cy : beginY,
+			r : radius
+		});
+		
+		sinAxis.attr({
+			x1: beginX,
+			y1: 0,
+			x2: beginX,
+			y2: window.innerHeight,
+			stroke: sinColor
+		});
+		
+		
+		cosAxis.attr({
+			x1: 0,
+			y1: beginY,
+			x2: window.innerWidth,
+			y2: beginY,
+			stroke: cosColor
+		});
+		
+		
+		tgAxis.attr({
+			x1: beginX + radius,
+			y1: 0,
+			x2: beginX + radius,
+			y2: window.innerHeight,
+			stroke: tgColor
+		});
+		
+		cotgAxis.attr({
+			x1: 0,
+			y1: beginY - radius,
+			x2: window.innerWidth,
+			y2: beginY - radius,
+			stroke: cotgColor
+		});
+		
+		angleArm.attr({
+			x1: beginX,
+			y1: beginY
+		});
+		
+		sinTxt.attr({
+			x: beginX + 10,
+			y: beginY - radius - 10,
+			fill: sinColor
+		});
+		
+		cosTxt.attr({
+			x: beginX + radius + 10,
+			y: beginY - 10,
+			fill: cosColor
+		})
+		
+		tgTxt.attr({
+			x: beginX + radius + 10,
+			y: beginY - (radius/2),
+			fill: tgColor
+		});
+		
+		cotgTxt.attr({
+			x: beginX + (radius / 2),
+			y : beginY - radius - 10,
+			fill: cotgColor
+		});
+		
+		
+		projectionSin.attr({
+			x1: beginX,
+			y1: beginY,
+			x2: beginX,
+			y2: beginY,
+			stroke: sinColor
+		});
+		
+		projectionCos.attr({
+			x1: beginX,
+			y1: beginY,
+			x2: beginX,
+			y2: beginY,
+			stroke: cosColor
+		});
+		
+	};
+	
+	showAdditions.click(function (){
+		if(showAdditions.is(":checked")){
+			projectionSin.attr("stroke-width","2");
+			projectionCos.attr("stroke-width","2");
+		}else{
+			projectionSin.attr("stroke-width","0");
+			projectionCos.attr("stroke-width","0");
+		}
 	});
 	
-	sinAxis.attr({
-		x1: beginX,
-		y1: 0,
-		x2: beginX,
-		y2: window.innerHeight,
-		stroke: sinColor
-	});
-	
-	
-	cosAxis.attr({
-		x1: 0,
-		y1: beginY,
-		x2: window.innerWidth,
-		y2: beginY,
-		stroke: cosColor
-	});
-	
-	
-	tgAxis.attr({
-		x1: beginX + radius,
-		y1: 0,
-		x2: beginX + radius,
-		y2: window.innerHeight,
-		stroke: tgColor
-	});
-	
-	cotgAxis.attr({
-		x1: 0,
-		y1: beginY - radius,
-		x2: window.innerWidth,
-		y2: beginY - radius,
-		stroke: cotgColor
-	});
-	
-	angleArm.attr({
-		x1: beginX,
-		y1: beginY
-	});
-	
-	sinTxt.attr({
-		x: beginX + 10,
-		y: beginY - radius - 10,
-		fill: sinColor
-	});
-	
-	cosTxt.attr({
-		x: beginX + radius + 10,
-		y: beginY - 10,
-		fill: cosColor
-	})
-	
-	tgTxt.attr({
-		x: beginX + radius + 10,
-		y: beginY - (radius/2),
-		fill: tgColor
-	});
-	
-	cotgTxt.attr({
-		x: beginX + (radius / 2),
-		y : beginY - radius - 10,
-		fill: cotgColor
-	});
-	
+	init();
 	
 	var angle = 0;
 	
-	
 	var animator;
-	
 	
 	animateAngle();
 	
 	function animateAngle(){
-		var toAngle = parseInt(box.val());
+		var toAngle = parseInt(angleBox.val());
 		
 		if(angle-toAngle > 0){
 			angle -= 1;
@@ -124,9 +163,19 @@ $(document).ready(function (){
 		var radians = angle * (Math.PI / 180);
 		
 		x = beginX + (Math.cos(radians) * radius);
-		
-		
 		y = beginY - (Math.sin(radians) * radius);
+		
+		projectionCos.attr({
+			x2: x,
+			y1: y,
+			y2: y
+		});
+		
+		projectionSin.attr({
+			x1: x,
+			x2: x,
+			y2: y
+		});
 		
 		
 		angleArm.attr("x2", x);
