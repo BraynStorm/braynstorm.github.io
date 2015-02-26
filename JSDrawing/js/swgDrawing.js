4a2b2780aa9004e1100a0f2f570cfd94ce210374
BraynStorm <glav0r3zzz4@gmail.com>
2015-02-26 18:26:24 +0200

    Fixed derps
    1. SIN / COS were showing switched
    2. Added TG / COTG values
    3. Fixed arc behaving wierdly with angles > -180 and < 0

--- JSDrawing/js/swgDrawing.js
+++ JSDrawing/js/swgDrawing.js
@@ -1,162 +1,212 @@
-$(document).ready(function (){
-	var angleBox	= $("#angleBox");
-	var radiusBox	= $("#radiusBox");
-	var beginX		= window.innerWidth/2;
-	var beginY		= window.innerHeight/2;
-	
-	var sinColor = "#00AA00";
-	var cosColor = "#0000AA";
-	var tgColor = "#00AAAA";
-	var cotgColor = "#AA0000";
-	
-	var radius		= 100;
-	
-	var svg			= $('svg');
-	
-	var circle		= $('#mainCircle');
-	var showAdditions = $("#showAdditions");
-	
-	var sinAxis 	= $("#sinAxis");
-	var cosAxis		= $("#cosAxis");
-	var tgAxis		= $("#tgAxis");
-	var cotgAxis	= $("#cotgAxis");
-	
-	var sinTxt 		= $("#sinTxt");
-	var cosTxt		= $("#cosTxt");
-	var tgTxt		= $("#tgTxt");
-	var cotgTxt		= $("#cotgTxt");
-	
-	
-	
-	var angleArm	= $("#angleArm");
-	var angleArc	= $("#angleArc");
-	var ghostArm	= $("#ghostArm");
-	
-	var projectionSin = $("#projectionSin");
-	var projectionCos = $("#projectionCos");
-	var projectionTg	= $("#projectionTg");
-	var projectionCotg	= $("#projectionCotg");
-	
-	var projectionSinTxt = $("#projectionSinTxt");
-	var projectionCosTxt = $("#projectionCosTxt");
-	radiusBox.keypress(function (){
-		init();
+var angleBox;
+var radiusBox;
+var beginX;
+var beginY;
+
+var sinColor;
+var cosColor;
+var tgColor;
+var cotgColor;
+
+var radius;
+
+var svg;
+
+var circle;
+var showAdditions;
+
+var sinAxis;
+var cosAxis;
+var tgAxis;
+var cotgAxis;
+
+var sinTxt;
+var cosTxt;
+var tgTxt;
+var cotgTxt;
+
+
+
+var angleArm;
+var angleArc;
+var ghostArm;
+
+var projectionSin;
+var projectionCos;
+var projectionTg;
+var projectionCotg;
+
+var projectionSinTxt;
+var projectionCosTxt;
+var projectionTgTxt;
+var projectionCotgTxt;
+
+var angle;
+var arcAngle;
+
+function init(){
+	radius = parseInt(radiusBox.val());
+	svg.css({
+		width:  window.innerWidth,
+		height: window.innerHeight
 	});
 	
-	function init(){
-		radius = parseInt(radiusBox.val());
-		svg.css({
-			width:  window.innerWidth,
-			height: window.innerHeight
-		});
-		
-		circle.attr({
-			cx : beginX,
-			cy : beginY,
-			r : radius
-		});
-		
-		sinAxis.attr({
-			x1: beginX,
-			y1: 0,
-			x2: beginX,
-			y2: window.innerHeight,
-			stroke: sinColor
-		});
-		
-		
-		cosAxis.attr({
-			x1: 0,
-			y1: beginY,
-			x2: window.innerWidth,
-			y2: beginY,
-			stroke: cosColor
-		});
-		
-		
-		tgAxis.attr({
-			x1: beginX + radius,
-			y1: 0,
-			x2: beginX + radius,
-			y2: window.innerHeight,
-			stroke: tgColor
-		});
-		
-		cotgAxis.attr({
-			x1: 0,
-			y1: beginY - radius,
-			x2: window.innerWidth,
-			y2: beginY - radius,
-			stroke: cotgColor
-		});
-		
-		angleArm.attr({
-			x1: beginX,
-			y1: beginY
-		});
-		
-		sinTxt.attr({
-			x: beginX + 10,
-			y: beginY - radius - 10,
-			fill: sinColor
-		});
-		
-		cosTxt.attr({
-			x: beginX + radius + 10,
-			y: beginY - 10,
-			fill: cosColor
-		})
-		
-		tgTxt.attr({
-			x: beginX + radius + 10,
-			y: beginY - (radius/2),
-			fill: tgColor
-		});
-		
-		cotgTxt.attr({
-			x: beginX + (radius / 2),
-			y : beginY - radius - 10,
-			fill: cotgColor
-		});
-		
-		
-		projectionSin.attr({
-			x1: beginX,
-			y1: beginY,
-			x2: beginX,
-			y2: beginY,
-			stroke: sinColor
-		});
-		
-		projectionCos.attr({
-			x1: beginX,
-			y1: beginY,
-			x2: beginX,
-			y2: beginY,
-			stroke: cosColor
-		});
-		
-		projectionTg.attr({
-			x1: beginX,
-			y1: beginY,
-			x2: beginX + radius,
-			y2: beginY,
-			stroke: tgColor
-		});
-		
-		projectionCotg.attr({
-			x1: beginX,
-			y1: beginY,
-			x2: beginX,
-			y2: beginY - radius,
-			stroke: cotgColor
-		});
-		
-		ghostArm.attr({
-			x1: beginX,
-			y1: beginY
-		});
-	};
+	circle.attr({
+		cx : beginX,
+		cy : beginY,
+		r : radius
+	});
+	
+	sinAxis.attr({
+		x1: beginX,
+		y1: 0,
+		x2: beginX,
+		y2: window.innerHeight,
+		stroke: sinColor
+	});
+	
+	
+	cosAxis.attr({
+		x1: 0,
+		y1: beginY,
+		x2: window.innerWidth,
+		y2: beginY,
+		stroke: cosColor
+	});
+	
+	
+	tgAxis.attr({
+		x1: beginX + radius,
+		y1: 0,
+		x2: beginX + radius,
+		y2: window.innerHeight,
+		stroke: tgColor
+	});
+	
+	cotgAxis.attr({
+		x1: 0,
+		y1: beginY - radius,
+		x2: window.innerWidth,
+		y2: beginY - radius,
+		stroke: cotgColor
+	});
+	
+	angleArm.attr({
+		x1: beginX,
+		y1: beginY
+	});
+	
+	sinTxt.attr({
+		x: beginX + 10,
+		y: beginY - radius - 10,
+		fill: sinColor
+	});
+	
+	cosTxt.attr({
+		x: beginX + radius + 10,
+		y: beginY - 10,
+		fill: cosColor
+	})
+	
+	tgTxt.attr({
+		x: beginX + radius + 10,
+		y: beginY - (radius/2),
+		fill: tgColor
+	});
+	
+	cotgTxt.attr({
+		x: beginX + (radius / 2),
+		y : beginY - radius - 10,
+		fill: cotgColor
+	});
+	
+	
+	projectionSin.attr({
+		x1: beginX,
+		y1: beginY,
+		x2: beginX,
+		y2: beginY,
+		stroke: sinColor
+	});
+	
+	projectionCos.attr({
+		x1: beginX,
+		y1: beginY,
+		x2: beginX,
+		y2: beginY,
+		stroke: cosColor
+	});
+	
+	projectionTg.attr({
+		x1: beginX,
+		y1: beginY,
+		x2: beginX + radius,
+		y2: beginY,
+		stroke: tgColor
+	});
+	
+	projectionCotg.attr({
+		x1: beginX,
+		y1: beginY,
+		x2: beginX,
+		y2: beginY - radius,
+		stroke: cotgColor
+	});
+	
+	
+	ghostArm.attr({
+		x1: beginX,
+		y1: beginY
+	});
+};
+
+$(document).ready(function (){
+	angleBox	= $("#angleBox");
+	radiusBox	= $("#radiusBox");
+	beginX		= window.innerWidth/2;
+	beginY		= window.innerHeight/2;
+	
+	sinColor = "#00AA00";
+	cosColor = "#0000AA";
+	tgColor = "#00AAAA";
+	cotgColor = "#AA0000";
+	
+	radius		= 100;
+	
+	svg			= $('svg');
+	
+	circle		= $('#mainCircle');
+	showAdditions = $("#showAdditions");
+	
+	sinAxis 	= $("#sinAxis");
+	cosAxis		= $("#cosAxis");
+	tgAxis		= $("#tgAxis");
+	cotgAxis	= $("#cotgAxis");
+	
+	sinTxt 		= $("#sinTxt");
+	cosTxt		= $("#cosTxt");
+	tgTxt		= $("#tgTxt");
+	cotgTxt		= $("#cotgTxt");
+	
+	
+	
+	angleArm	= $("#angleArm");
+	angleArc	= $("#angleArc");
+	ghostArm	= $("#ghostArm");
+	
+	projectionSin = $("#projectionSin");
+	projectionCos = $("#projectionCos");
+	projectionTg	= $("#projectionTg");
+	projectionCotg	= $("#projectionCotg");
+	
+	projectionSinTxt = $("#projectionSinTxt");
+	projectionCosTxt = $("#projectionCosTxt");
+	projectionTgTxt = $("#projectionTgTxt");
+	projectionCotgTxt = $("#projectionCotgTxt");
+	
+	radiusBox.keyup(function (){
+		init();
+	});
 	
 	showAdditions.click(function (){
 		if(showAdditions.is(":checked")){
@@ -180,9 +230,10 @@ $(document).ready(function (){
 		}
 	});
 	
+	
 	init();
 	
-	var angle = 0;
+	angle = 0;
 	
 	var animator;
 	
@@ -240,18 +291,26 @@ $(document).ready(function (){
 		projectionSinTxt.attr({
 			x: x+10,
 			y: (beginY - (beginY - y) / 2) + (parseInt(projectionSinTxt.css("font-size")) / 2)
-		}).html(parseInt(cos*100)/100);
+		}).html(parseInt(sin*100)/100);
 		
 		projectionCosTxt.attr({
 			x: (beginX + (x - beginX) / 2) - 10,
 			y: y - 10
-		}).html(parseInt(sin*100)/100);
+		}).html(parseInt(cos*100)/100);
 		
-		if(angle % 360 > 90 && angle % 360 < 360){
-			console.log(angle % 360);
+		projectionTgTxt.attr({
+			x: beginX + radius + 10,
+			y: Math.max(20, Math.min(beginY - tg * radius, window.innerHeight - 20))
+		}).html(parseInt(tg * 100) / 100);
+		
+		projectionCotgTxt.attr({
+			x: Math.max(10, Math.min(beginX + cotg * radius, window.innerWidth - 55)),
+			y: beginY - radius - 10
+		}).html(parseInt(cotg * 100) / 100);
+		
+		if(!(sin > 0 && cos > 0)){
 			ghostArm.attr("stroke-width", "2");
 		}else{
-			console.log("asdasd");
 			ghostArm.attr("stroke-width", "0");
 		}
 		
@@ -265,34 +324,52 @@ $(document).ready(function (){
 			y2: beginY - ( Math.sin((180 + angle % 360) * (Math.PI / 180)) * radius * 4)
 		});
 		
-		angleArc.attr("d", describeArc(beginX, beginY, radius/5, 90-(angle%360), 90));
+		arcAngle = (90-angle % 360);
+		
+		angleArc.attr("d", describeArc(beginX, beginY, radius/5, Math.min(arcAngle,90), Math.max(90,arcAngle)));
 		
 		
 		animator = setTimeout(animateAngle, 15);
 	}
 	
-	function describeArc(x, y, radius, startAngle, endAngle){
-
-	    var start = polarToCartesian(x, y, radius, endAngle);
-	    var end = polarToCartesian(x, y, radius, startAngle);
-
-	    var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
-
-	    var d = [
-	        "M", start.x, start.y, 
-	        "A", radius, radius, 0, arcSweep, 0, end.x, end.y
-	    ].join(" ");
-
-	    return d;       
-	}
-	function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-		  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-
-		  return {
-		    x: centerX + (radius * Math.cos(angleInRadians)),
-		    y: centerY + (radius * Math.sin(angleInRadians))
-		  };
+	function getQuadrant(a){
+		a = a % 360;
+		if(a >= 0){
+			if(a <= 90) return 1;
+			if(a <= 180) return 2;
+			if(a <= 270) return 3;
+			return 4;
+		}else{
+			if(a >= -90) return 4;
+			if(a >= -180) return 3;
+			if(a >= -270) return 2;
+			return 1;
 		}
+	}
+	
+	function describeArc(x, y, radius, startAngle, endAngle){
+		
+		var start = polarToCartesian(x, y, radius, endAngle);
+		var end = polarToCartesian(x, y, radius, startAngle);
+		
+		var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+		
+		var d = [
+		    "M", start.x, start.y, 
+		    "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+		].join(" ");
+		
+		return d;       
+	}
+	
+	function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+		var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+		
+		return {
+		  x: centerX + (radius * Math.cos(angleInRadians)),
+		  y: centerY + (radius * Math.sin(angleInRadians))
+		};
+	}
 
 });
 
