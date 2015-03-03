7a805ad440b55be3eff83647e34157e388882e05
BraynStorm <glav0r3zzz4@gmail.com>
2015-03-03 14:55:12 +0200

    Started complete rewrite of root

--- root/main.js
+++ root/main.js
@@ -1,63 +1,43 @@
 $(document).ready(function (){
-	var button = $(".button");
-	var leftSide = ".buttonLeftSide";
-	var rightSide = ".buttonRightSide";
-	var closed = ".buttonClosed";
-	var content;
-	var buttonWidth = '500';
-	
-	
-	button.hover(function (){
-		content = $(this).find(".buttonContent");
-		
-		$(this).find(closed).stop().animate({
-			//transform : "rotate(0deg)"
-		}, {duration: 500, done: function (){
-			//$(this).css("display", "none");
-			
-			$(this).parent().find(leftSide).stop().animate({
-				marginLeft: parseInt($(this).width())/2 - parseInt($(this).find(leftSide).width()) - buttonWidth/2 + 20
-			}, {duration: 700, queue: false, start: function (){
-				$(this).css("display", "block");
-			}});
-			
-			$(this).parent().find(rightSide).stop().animate({
-				marginLeft: 0
-			}, {duration: 700, queue: false, start: function (){
-				$(this).css("display", "block");
-			}});
-			
-			content.stop().animate({
-				width: buttonWidth,
-				paddingLeft: 30,
-				paddingRight: 30,
-			}, {duration: 700, queue: false});
-		}});
-		
-		
-		
-		
-		
-	}, function (){
-		content = $(this).find(".buttonContent");
-
-		$(this).find(leftSide).stop().animate({
-			marginLeft: parseInt($(this).width())/2 - parseInt($(this).find(leftSide).width())
-		}, {duration: 700, queue:false, done: function (){
-			$(this).css("display", "none");
-		}});
-		
-		$(this).find(rightSide).stop().animate({
-			marginLeft: -20
-		}, {duration: 700, queue:false, done: function (){
-			$(this).css("display", "none");
-		}});
-		
-		content.stop().animate({
-			width: 0,
-			paddingLeft: 0,
-			paddingRight: 0
-		}, {duration: 700, queue: false});
-	}).trigger();
-	
+	OpeningButton.init(".button-holder",".button-spinner",".button-left",".button-right");
 });
+
+var OpeningButton = {};
+
+OpeningButton.init = function (buttonClass, spinnerClass, buttonLeftClass, buttonRightClass){
+	
+	if($ == undefined || jQuery == undefined){
+		console.log("JQUERY isn't installed!" + "\n Exiting..");
+		return 0;
+	}
+	
+	THIS = this;
+	
+	var button  = $(buttonClass);
+	var spinner;
+	var leftSide;
+	var rightSide;
+	
+	button.mouseover(function (){
+		spinner = $(this).find('div' + spinnerClass);
+		
+		spinner.stop().transition({rotate:'90deg', duration: 500, complete: function (){
+			spinner.css("display", 'none');
+		}});
+		//spinner.css("display", 'none');
+	}).mouseout(function (){
+		spinner = $(this).find('div' + spinnerClass);
+		
+		
+		
+		spinner.stop().css("display", 'block').transition({
+			rotate:'0deg',
+			duration: 500,
+			complete: function (){
+				spinner.css("display", 'block');
+			}
+		});
+		
+	});
+	
+}
